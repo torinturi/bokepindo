4fae7d224744366566327fedba5cdace2189f6c3
torinturi <160316932+torinturi@users.noreply.github.com>
2024-03-20 03:01:19 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const font = Nunito({ subsets: ["latin"] });
 export const metadata: Metadata = {
     title: `${SITENAME} Video Bokep Indo Jepang Barat Viral Terbaru`,
     description: `${SITENAME} Video bokep indo jepang indonesia barat terbaru viral korea simontok china tante live sma paksa ngentot abg jilbab cewek bocil smp pijat pelajar.`,
-    metadataBase: new URL("https://bokepsmp.pages.dev"),
+    metadataBase: new URL("https://bokepind.pages.dev"),
     alternates: {
         canonical: `/`,
     },
